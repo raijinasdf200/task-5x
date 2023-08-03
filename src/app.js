c554b8b22f4469cbde0330c9b46bb3fd95c68ab2
Mohamed Rezk <127786604+raijinasdf200@users.noreply.github.com>
2023-08-04 01:48:59 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 // -------------------------------------------------
 const express= require('express')
 const app=express()
-const port=process.env.port || 5000
+const port=process.env.PORT || 5000
 
 const path = require('path')
 const publicDirectory =path.join(__dirname,'../public')
